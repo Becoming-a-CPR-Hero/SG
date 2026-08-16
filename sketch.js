2cf3882e1bd0488cb57656b613e6f9692c1e072e
Becoming-a-CPR-Hero <cprrajarani@gmail.com>
2026-08-16 11:05:20 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -118,7 +118,7 @@ let compression_count = 0;
 let now,interval;
 let lastTouchTime = 0;
 // log into google sheets - google app script
-const scriptURL = "https://script.google.com/macros/s/AKfycbxNVQSYjwBKOwIT8stzs-7oS4mOBfTWHkVzP_e0tr3QRfTm4_imeTxxDbV9qVfJclPLKg/exec";
+const scriptURL = "https://script.google.com/macros/s/AKfycbyPz-C6Y1jgHc96q_k_NqSktpVdGsqHPmp0FxH1h5N2GT7crTUwkWk1dNl07hAFgaM9xQ/exec";
 let sessionLogged = false;
 // progress tracking (compression score history, saved to localStorage)
 let scoreLoggedForAttempt = false;  // guards against logging the same attempt multiple times
